d97edd2f8f637bcd5afef1c771d788bf32a22d21
Comparekink <admin@comparekink.com>
2025-03-08 14:56:30 +0000

    wip

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash 
+
+nix-env -iA \
+nixpkgs.stow \
+nixpkgs.kakoune \
+nixpkgs.tig \
+nixpkgs.nnn \
+nixpkgs.htop \
+nixpkgs.zellij
+# nixpkgs.fzf 
+
+stow tig
+stow zellij
+stow kak
+stow bash
+stow git
